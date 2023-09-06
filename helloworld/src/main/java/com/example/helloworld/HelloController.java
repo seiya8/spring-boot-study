5fa1f0ac63ef578ccc9f61ca42c736f9ca3ec8e1
seiya8 <seiya.sasa8@gmail.com>
2023-09-06 17:36:52 +0900

    フォームで名前を送信してhello nameと表示

--- helloworld/src/main/java/com/example/helloworld/HelloController.java
+++ helloworld/src/main/java/com/example/helloworld/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.helloworld;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @RequestMapping(value="/", method=RequestMethod.GET)
-    public String hell() {
+    @GetMapping("/")
+    public String index() {
+        return "hello";
+    }
+    @PostMapping("/")
+    String postName(@RequestParam("name") String responseVal, Model model) {
+        model.addAttribute("name", responseVal);
         return "hello";
     }
 }
